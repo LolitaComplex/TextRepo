bcda1708b1ed786e3c35401df824943cbecc637c
LolitaComplex <1024587616@qq.com>
2017-07-21 11:44:56 +0800

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+/**
+ * Doing
+ */
+public class Doing {
+
+    public Doing (parameters) {
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Test Git");
+    }   
+}
